798434aa097b476d4cb31581e65c9d18d2926a8c
ZrisanDev <gabriel.andrade.0296@gmail.com>
2026-08-06 12:22:07 -0500

    feat(shared): add sidebar, breadcrumb, tooltip, avatar, collapsible, sheet primitives from sidebar-07 block

--- packages/shared/src/ui/index.ts
+++ packages/shared/src/ui/index.ts
@@ -17,15 +17,21 @@
  */
 
 export * from "./alert";
+export * from "./avatar";
 export * from "./badge";
+export * from "./breadcrumb";
 export * from "./button";
 export * from "./card";
+export * from "./collapsible";
 export * from "./dialog";
 export * from "./dropdown-menu";
 export * from "./input";
 export * from "./label";
 export * from "./select";
 export * from "./separator";
+export * from "./sheet";
+export * from "./sidebar";
 export * from "./skeleton";
 export * from "./sonner";
 export * from "./table";
+export * from "./tooltip";
